b01fc3a1644d45c2878230290c08f486d4273072
Muhammad Fajri Yosri <63485206+semogaBerkahh@users.noreply.github.com>
2021-12-07 01:00:07 +0800

    Update Service1.cs

--- ServiceReservasi/Service1.cs
+++ ServiceReservasi/Service1.cs
@@ -7,7 +7,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace ServiceReservasi_119
+namespace ServiceReservasi
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
